2f6f33578155df1a8b59e69868f2f66aded6bdf5
Vincent Leroy <vincent.leroy@rayference.eu>
2021-01-17 19:28:27 +0100

    Added come package metadata

--- src/pinttr/__init__.py
+++ src/pinttr/__init__.py
@@ -1,6 +1,14 @@
 from . import converters, exceptions, util, validators
 from ._make import attrib
 
-__all__ = ["attrib", "ib", "converters", "exceptions", "util", "validators"]
 
+# Package metadata
+__version__ = "0.1.0-dev"
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2021 Vincent Leroy"
+
+# Other definitions
 ib = attrib
+
+__all__ = ["attrib", "ib", "converters", "exceptions", "util", "validators"]
