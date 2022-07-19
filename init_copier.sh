8023cef5c95e5231f321ae91bbeebeeacd711e11
Vincent Leroy <vincent.leroy@rayference.eu>
2022-07-19 14:03:10 +0200

    Apply copier template

--- init_copier.sh
+++
@@ -1,8 +0,0 @@
-copier -f --prereleases ../copier-pdm . \
-    -d project_name="Pinttrs" \
-    -d project_description="Pint meets attrs." \
-    -d author_fullname="Vincent Leroy" \
-    -d author_username="leroyvn" \
-    -d author_email="vincent.leroy@rayference.eu" \
-    -d python_package_import_name="pinttr" \
-    -d python_package_distribution_name="pinttrs"
