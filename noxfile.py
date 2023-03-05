72b10c54ed14478ca2390424e8cf779fc3751ba6
Vincent Leroy <vincent.leroy@rayference.eu>
2023-03-05 16:34:29 +0100

    CI: Use stock Nox setup action

--- noxfile.py
+++ noxfile.py
@@ -7,7 +7,7 @@ def _has_venv(session):
     return not isinstance(session.virtualenv, nox.virtualenv.PassthroughEnv)
 
 
-@nox.session(python=("3.7", "3.8", "3.9", "3.10"))
+@nox.session(python=("3.8", "3.9", "3.10", "3.11"))
 def test(session):
     """Run the test suite."""
 
